65b53bc8f59ac5e822863701bac8aa6827cc652f
Afrisal Yodi Purnama <afrisalyp@gmail.com>
2020-03-27 09:40:46 +0700

    Handle multiple repo in db.

--- routes/middlewares/SecurityChecker.js
+++ routes/middlewares/SecurityChecker.js
@@ -9,6 +9,7 @@
  */
 'use strict';
 const crypto = require('crypto');
+const _ = require('lodash');
 const logger = require('../../utils/logger');
 const Project = require('../../models').Project;
 const dbHelper = require('../../utils/db-helper');
@@ -17,17 +18,24 @@ module.exports = (provider) => async (req, res, next) => {
   let isValid = false;
   const params = req.body;
   if (provider === 'github') {
-    const projectDetail = await dbHelper.scanOne(Project, {
+    const projectDetails = await dbHelper.scan(Project, {
       repoUrl: params.repository.html_url
     });
-
-    const hash = crypto.createHmac('sha1', projectDetail.secretWebhookKey).update(req.rawBody).digest('hex');
-    isValid = `sha1=${hash}` === req.header('X-Hub-Signature');
+    _.forEach(projectDetails, (projectDetail) => {
+      const hash = crypto.createHmac('sha1', projectDetail.secretWebhookKey).update(req.rawBody).digest('hex');
+      if (`sha1=${hash}` === req.header('X-Hub-Signature')) {
+        isValid = true;
+      }
+    });
   } else if (provider === 'gitlab') {
-    const projectDetail = await dbHelper.scanOne(Project, {
+    const projectDetails = await dbHelper.scan(Project, {
       repoUrl: params.project.web_url
     });
-    isValid = projectDetail.secretWebhookKey === req.header('X-Gitlab-Token');
+    _.forEach(projectDetails, (projectDetail) => { // eslint-disable-line lodash/prefer-filter
+      if (projectDetail.secretWebhookKey === req.header('X-Gitlab-Token')) {
+        isValid = true;
+      }
+    });
   } else {
     // unknown provider
     return next();
