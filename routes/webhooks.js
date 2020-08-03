0733c87dc4571589db918d032b19053f7451a450
Afrisal Yodi Purnama <afrisalyp@gmail.com>
2020-08-03 20:08:58 +0700

    Remove azure.

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -14,7 +14,6 @@ const logger = require('../utils/logger');
 const kafka = require('../utils/kafka');
 const GithubEventDetector = require('../utils/GithubEventDetector');
 const GitlabEventDetector = require('../utils/GitlabEventDetector');
-const AzureEventDetector = require('../utils/AzureEventDetector');
 
 const RepositoryFilter = require('./middlewares/RepositoryFilter');
 const SecurityChecker = require('./middlewares/SecurityChecker');
@@ -44,21 +43,6 @@ router.post('/gitlab', SecurityChecker('gitlab'), RepositoryFilter('gitlab'), wr
     await kafka.send(JSON.stringify(result));
     logger.info(`successfully add event: ${result.event} to kafka queue`);
     logger.debug(`kafka message: ${JSON.stringify(result)}`);
-  }
-  res.json({success: true});
-}));
-
-router.post('/azure', SecurityChecker('azure'), RepositoryFilter('azure'), wrapper(async (req, res) => {
-  const result = AzureEventDetector.detectMany(req.body);
-  if (!result || result.length === 0) {
-    logger.info('unknown event detected');
-    logger.debug(req.body);
-  } else {
-    for (const event of result) { // eslint-disable-line no-restricted-syntax
-      await kafka.send(JSON.stringify(event));
-      logger.info(`successfully add event: ${event.event} to kafka queue`);
-      logger.debug(`kafka message: ${JSON.stringify(event)}`);
-    }
   }
   res.json({success: true});
 }));
